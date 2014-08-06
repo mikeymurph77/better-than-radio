16b5b04d6f083fe959574761a5d3760d31103742
Mike Murphy <mikeymurph77@gmail.com>
2014-08-06 11:46:32 -0400

    Change redirect for fan creation to root_path

--- better_than_radio/app/controllers/fans_controller.rb
+++ better_than_radio/app/controllers/fans_controller.rb
@@ -10,7 +10,7 @@ class FansController < ApplicationController
 
     if @fan.save
       current_user.update(account: @fan)
-      redirect_to :fan_dashboard
+      redirect_to root_path
     else
       render :new
     end
