67fcf91e8086128a0e65b11eb70c0ee1956a518d
Mike Murphy <mikeymurph77@gmail.com>
2014-08-13 15:27:54 -0400

    Fix Hound comments on whitespace

--- better_than_radio/app/uploaders/image_uploader.rb
+++ better_than_radio/app/uploaders/image_uploader.rb
@@ -1,8 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
-  
   storage :file
-  
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
