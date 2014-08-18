d0600d5ece944bbb1a0e406c89e7e260fa2de347
Mike Murphy <mikeymurph77@gmail.com>
2014-08-18 15:24:12 -0400

    Remove pg_search migration

--- better_than_radio/db/schema.rb
+++ better_than_radio/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140818164054) do
+ActiveRecord::Schema.define(version: 20140814211902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,14 +65,6 @@ ActiveRecord::Schema.define(version: 20140818164054) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "profile_picture"
-  end
-
-  create_table "pg_search_documents", force: true do |t|
-    t.text     "content"
-    t.integer  "searchable_id"
-    t.string   "searchable_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "photos", force: true do |t|
